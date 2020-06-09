cf10c07c0481550708582a436968f0c1f573f513
Sergei Rybalko <sergei@core>
2020-06-09 19:34:00 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+with open('README.md',"r") as f:
+    long_description = f.read()
+
+setup(name='topic_class',
+      version='0.1.0',
+      description='This package consists of Topic Model class ',
+      url='https://github.com/Benja1972/topic_class',
+      author='Sergei Rybalko',
+      author_email='benja1972@gmail.com',
+      license='MIT',
+      packages=['topic_class'],
+      install_requires=['gensim', 'numpy', 'joblib'],
+      python_requires='>=3.6',
+      include_package_data=True,
+      zip_safe=True)
